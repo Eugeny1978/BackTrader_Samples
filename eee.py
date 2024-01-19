aa21338aa5b6f74b1c1e664d6851d950069f7e39
Eugeny1978 <luchnik78@mail.ru>
2024-01-19 17:18:31 +0300

    Add Files

---
+++ eee.py
@@ -0,0 +1,15 @@
+import backtrader as bt
+
+# bt.feeds.BacktraderCSVData()
+# bt.feeds.TimeFrame()
+# bt.feeds.VChartFeed()
+# bt.feeds.BacktraderCSV()
+# bt.feeds.GenericCSVData()
+# bt.feeds.GenericCSV()
+# bt.feeds.PandasData()
+# bt.feeds.PandasDirectData()
+# bt.feeds.VChartCSVData()
+# bt.Cerebro()
+
+params = dict(sma_period = 21, ama_period = 14)
+print(params)
