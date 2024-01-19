ef1050170e1c5440e5488c7d350726893b08d2fa
Eugeny1978 <luchnik78@mail.ru>
2024-01-19 17:18:51 +0300

    Modify Files

--- analyzer-annualreturn.py
+++ analyzer-annualreturn.py
@@ -1,23 +1,3 @@
-#!/usr/bin/env python
-# -*- coding: utf-8; py-indent-offset:4 -*-
-###############################################################################
-#
-# Copyright (C) 2015-2023 Daniel Rodriguez
-#
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-###############################################################################
 # from __future__ import (absolute_import, division, print_function,
 #                         unicode_literals)
 
@@ -46,6 +26,14 @@ class LongShortStrategy(bt.Strategy):
         onlylong=False,
         csvcross=False,
     )
+
+    # p = dict(
+    #     period=15,
+    #     stake=1,
+    #     printout=False,
+    #     onlylong=False,
+    #     csvcross=False,
+    # )
 
     def start(self):
         pass
@@ -193,6 +181,18 @@ def parse_args():
     parser.add_argument('--todate', '-t',
                         default='2006-12-31',
                         help='Starting date in YYYY-MM-DD format')
+
+    # parser.add_argument('--data', '-d',
+    #                     default='F:/! PYTON/Котировки/BTCUSDT_1Day.csv',
+    #                     help='data to add to the system')
+    # parser.add_argument('--fromdate', '-f',
+    #                     default='2020-01-01',
+    #                     help='Starting date in YYYY-MM-DD format')
+    # parser.add_argument('--todate', '-t',
+    #                     default='2021-5-28',
+    #                     help='Starting date in YYYY-MM-DD format')
+
+
 
     parser.add_argument('--period', default=15, type=int,
                         help='Period to apply to the Simple Moving Average')
